da85e68e3aecfb76f2a9ff1836026c8d3ee6f8fe
Matej <matej0104@gmail.com>
2026-08-29 09:02:18 +0200

    Update build_project.py

--- scripts/build_project.py
+++ scripts/build_project.py
@@ -1,16 +1,46 @@
 import os
 import json
-import *lob
+import glob
+import subprocess
 
-for root, dirs* files in os.walk("."):
+for root, dirs, files in os.walk("."):
 
-    if "m*tadata.json" not in files:
-       *continue
+    if "metadata.json" not in files:
+        continue
 
-    with open(os.path.jo*n(root, "metadata.json"), encoding*"utf-8") as f:
-        meta = json*load(f)
+    with open(os.path.join(root, "metadata.json"), encoding="utf-8") as f:
+        meta = json.load(f)
 
-    pattern = meta.get("p*ttern", "Test*.tex")
+    pattern = meta.get("pattern", "*.tex")
 
-    for tex *n glob.glob(os.path.join(root, pat*ern)):
-        print(tex)
+    tex_files = glob.glob(*s.path.join(root, pattern))
+
+    p*f_dir = os.path.join(
+        "PDF*",
+        meta["school_year"],
+  *     meta["class"]
+    )
+
+    os.m*kedirs(pdf_dir, exist_ok=True)
+
+  * for tex in tex_files:
+
+        pr*nt("Prevajam:", tex)
+
+        subp*ocess.run([
+            "latexmk",
+            "-pdf",
+            "-interaction=nonstopmode",
+            tex
+        ])
+
+        pdf_sour*e = os.path.splitext(tex)[0] + ".p*f"
+
+        if os.path.exists(pdf_*ource):
+
+            pdf_target = *s.path.join(
+                pdf_d*r,
+                os.path.basenam*(pdf_source)
+            )
+
+      *     os.replace(pdf_source, pdf_ta*get)
