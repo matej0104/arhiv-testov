7a16225d8c405e35913631547005eef625e7c551
Matej <matej0104@gmail.com>
2026-08-29 09:28:57 +0200

    Update sync.bat

--- sync.bat
+++ sync.bat
@@ -1,2 +1,25 @@
+@echo off
+
+echo ===================================
+echo OVERLEAF -> GITHUB SYNC
+echo ===================================
+
 git pull origin master
+
+if errorlevel 1 (
+    echo Napaka pri prenosu iz Overleafa.
+    pause
+    exit /b 1
+)
+
 git push github main
+
+if errorlevel 1 (
+    echo Napaka pri posiljanju na GitHub.
+    pause
+    exit /b 1
+)
+
+echo.
+echo Sinhronizacija uspesna.
+pause
